50ac0665808d738a96005e5809672067763f8605
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-11 19:40:50 +0100

    Saved SQL commands to variables

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,20 +3,24 @@ require 'pg'
 class Peep
 
   attr_reader :text
+  @prod_db = PG.connect(dbname: 'chitter')
+  @test_db = PG.connect(dbname: 'chitter_test')
+  @save_peep = "INSERT INTO peeps (text) VALUES($1) RETURNING id, text;"
+  @get_peeps = "SELECT * FROM peeps;"
 
   def initialize(text:)
     @text = text
   end
 
   def self.create(text:)
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'chitter_test') : connection = PG.connect(dbname: 'chitter')
-    result = connection.exec_params('INSERT INTO peeps (text) VALUES($1) RETURNING id, text;', [text])
+    ENV['ENVIRONMENT'] == 'test' ? connection = @test_db : connection = @prod_db
+    result = connection.exec_params(@save_peep, [text])
     Peep.new(text: result[0]['text'])
   end
 
   def self.all
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'chitter_test') : connection = PG.connect(dbname: 'chitter')
-    result = connection.exec_params("SELECT * FROM peeps;")
+    ENV['ENVIRONMENT'] == 'test' ? connection = @test_db : connection = @prod_db
+    result = connection.exec_params(@get_peeps)
     result.map do |peep|
       Peep.new(text: peep['text'])
     end
