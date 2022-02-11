91dac5f5499f306d8664bfde952c615905e6669c
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-11 14:29:05 +0100

    /peeps route now directs to erb file

--- app.rb
+++ app.rb
@@ -11,11 +11,12 @@ class Chitter < Sinatra::Base
   end
 
   get '/peeps' do
-    peeps = [
+    @peeps = [
       "Hobnobs are the best biscuit",
       "Fun fact: the can opener was invented over 1000 years before the can",
       "Ignorance, the root and stem of every evil. ― Plato"
     ]
+    erb :'peeps/index'
   end
 
   run! if app_file == $0
