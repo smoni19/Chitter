b294fba9ed2c0684524e5b0c62e989b82bacc1e0
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-25 13:03:36 +0100

    Changed css colours to variables. Styled delete link hover

---
+++ public/javascripts/change_border.js
@@ -0,0 +1,7 @@
+function RedBorder(obj) {
+  obj.parentElement.parentElement.classList.add("peep_delete");
+}
+
+function NormalBorder(obj) {
+  obj.parentElement.parentElement.classList.remove("peep_delete");
+}
