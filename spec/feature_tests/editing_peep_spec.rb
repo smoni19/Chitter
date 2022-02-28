c79e31893b98620d551c77426963a41d97de378d
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-28 20:48:03 +0100

    Users can edit Peeps

--- spec/feature_tests/editing_peep_spec.rb
+++ spec/feature_tests/editing_peep_spec.rb
@@ -7,11 +7,11 @@ feature "users can edit a Peep" do
     signup("John Smith", "js2000", "js2000@test.com", "1234")
     post_peep("This is a test peep")
     click_button("Edit")
-    fill_in(:edited_text, with: "This is an edited Peep")
+    fill_in(:edited_text, with: "This peep has been edited")
     click_button("Save Peep")
     expect(page.status_code).to eq 200
     expect(page).to have_content("Edited")
-    expect(page).to have_content("This is an edited Peep")
+    expect(page).to have_content("This peep has been edited")
   end
   
 end
